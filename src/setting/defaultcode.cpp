8bb59c07e52f298a75f431cb84e161c9ebc18775
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2024-11-20 21:04:45 +0900

    Go jinhan814

--- src/setting/defaultcode.cpp
+++ src/setting/defaultcode.cpp
@@ -66,4 +66,14 @@ uint64_t splitmix64(uint64_t x) {
     x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
     return x ^ (x >> 31);
 }
+vector e(m, tuple(0, 0, 0));
+for (auto& [a, b, c] : e) cin >> a >> b >> c;
+vector cnt(n + 2, 0); vector csr(m, pair(0, 0));
+for (auto [a, b, c] : e) cnt[a + 1]++;
+for (int i = 1; i < cnt.size(); i++) cnt[i] += cnt[i - 1];
+for (auto [a, b, c] : e) csr[cnt[a]++] = pair(b, c);
+int cur = /* ... */;
+for (int i = cnt[cur - 1]; i < cnt[cur]; i++) {
+  auto [nxt, cost] = csr[i]; /* ... */
+}
 arr.reserve(n) // 공간 미리 할당 + push_back 사용

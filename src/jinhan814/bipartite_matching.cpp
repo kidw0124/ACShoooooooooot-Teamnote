dd0f0560811211738ae97d73f8f4030b59893b9e
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-11-20 20:30:51 +0900

    added csr and bipartite matching

---
+++ src/jinhan814/bipartite_matching.cpp
@@ -0,0 +1,17 @@
+auto bipartite_matching = [](const auto& adj) {
+  const int n = adj.size() - 1;
+  vector par(n + 1, 0), c(n + 1, 0);
+  auto dfs = [&](const auto& self, int cur) -> bool {
+    if (c[cur]++) return 0;
+    for (int nxt : adj[cur])
+      if (!par[nxt] || self(self, par[nxt]))
+        return par[nxt] = cur, 1;
+    return 0;
+  };
+  int res = 0;
+  for (int i = 1; i <= n; i++) {
+    fill(c.begin(), c.end(), 0);
+    if (dfs(dfs, i)) res++;
+  }
+  return res;
+};

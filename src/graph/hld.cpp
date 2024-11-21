36270a8c36ecc0a38ff23615b45a04ca87c6f373
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2024-11-22 05:27:45 +0900

    Combine jinhan814's

--- src/graph/hld.cpp
+++ src/graph/hld.cpp
@@ -1,110 +1,28 @@
-// heavy-light decomposition
-//
-// hld h;
-// insert edges to tree[0~n-1];
-// h.init(n, root);
-// h.decompose(root);
-// h.hldquery(u, v); // edges from u to v
-struct hld {
-    static const int MAXLN = 18;
-    static const int MAXN = 1 << (MAXLN - 1);
-    vector<int> tree[MAXN];
-    int subsize[MAXN], depth[MAXN], pa[MAXLN][MAXN];
-
-    int chead[MAXN], cidx[MAXN];
-    int lchain;
-    int flatpos[MAXN + 1], fptr;
-
-    void dfs(int u, int par) {
-        pa[0][u] = par;
-        subsize[u] = 1;
-        for (int v : tree[u]) {
-            if (v == pa[0][u]) continue;
-            depth[v] = depth[u] + 1;
-            dfs(v, u);
-            subsize[u] += subsize[v];
-        }
+// heavy-light decomposition in O(n)
+auto get_hld = [](auto adj) {
+  const int n = adj.size() - 1;
+  int ord = 0;
+  vector sz(n + 1, 1), dep(n + 1, 0), par(n + 1, 0);
+  vector in(n + 1, 0), out(n + 1, 0), top(n + 1, 0);
+  auto dfs1 = [&](const auto& self, int cur, int prv) -> void {
+    if (prv) adj[cur].erase(ranges::find(adj[cur], prv));
+    for (int& nxt : adj[cur]) {
+      dep[nxt] = dep[cur] + 1;
+      par[nxt] = cur;
+      self(self, nxt, cur);
+      sz[cur] += sz[nxt];
+      if (sz[adj[cur][0]] < sz[nxt]) swap(adj[cur][0], nxt);
     }
-
-    void init(int size, int root)
-    {
-        lchain = fptr = 0;
-        dfs(root, -1);
-        memset(chead, -1, sizeof(chead));
-
-        for (int i = 1; i < MAXLN; i++) {
-            for (int j = 0; j < size; j++) {
-                if (pa[i - 1][j] != -1) {
-                    pa[i][j] = pa[i - 1][pa[i - 1][j]];
-                }
-            }
-        }
+  };
+  auto dfs2 = [&](const auto& self, int cur) -> void {
+    in[cur] = ++ord;
+    for (int nxt : adj[cur]) {
+      top[nxt] = adj[cur][0] == nxt ? top[cur] : nxt;
+      self(self, nxt);
     }
-
-    void decompose(int u) {
-        if (chead[lchain] == -1) chead[lchain] = u;
-        cidx[u] = lchain;
-        flatpos[u] = ++fptr;
-
-        int maxchd = -1;
-        for (int v : tree[u]) {
-            if (v == pa[0][u]) continue;
-            if (maxchd == -1 || subsize[maxchd] < subsize[v]) maxchd = v;
-        }
-        if (maxchd != -1) decompose(maxchd);
-
-        for (int v : tree[u]) {
-            if (v == pa[0][u] || v == maxchd) continue;
-            ++lchain; decompose(v);
-        }
-    }
-
-    int lca(int u, int v) {
-        if (depth[u] < depth[v]) swap(u, v);
-
-        int logu;
-        for (logu = 1; 1 << logu <= depth[u]; logu++);
-        logu--;
-
-        int diff = depth[u] - depth[v];
-        for (int i = logu; i >= 0; --i) {
-            if ((diff >> i) & 1) u = pa[i][u];
-        }
-        if (u == v) return u;
-
-        for (int i = logu; i >= 0; --i) {
-            if (pa[i][u] != pa[i][v]) {
-                u = pa[i][u];
-                v = pa[i][v];
-            }
-        }
-        return pa[0][u];
-    }
-
-    // TODO: implement query functions
-    inline int query(int s, int e) {
-        return 0;
-    }
-
-    int subquery(int u, int v) {
-        int uchain, vchain = cidx[v];
-        int ret = 0;
-        for (;;) {
-            uchain = cidx[u];
-            if (uchain == vchain) {
-                ret += query(flatpos[v], flatpos[u]);
-                break;
-            }
-
-            ret += query(flatpos[chead[uchain]], flatpos[u]);
-            u = pa[0][chead[uchain]];
-        }
-        return ret;
-    }
-
-    inline int hldquery(int u, int v) {
-        int p = lca(u, v);
-        return subquery(u, p) + subquery(v, p) - query(flatpos[p], flatpos[p]);
-    }
+    out[cur] = ord;
+  };
+  dfs1(dfs1, 1, 0);
+  dfs2(dfs2, top[1] = 1);
+  return tuple(sz, dep, par, in, out, top);
 };
-
--- src/math/chinese-remainder.cpp
+++ src/math/chinese-remainder.cpp
@@ -1,17 +1,19 @@
-// find x s.t.  x === a[0] (mod n[0])
-//                === a[1] (mod n[1])
-//                ...
-// assumption: gcd(n[i], n[j]) = 1
-ll chinese_remainder(ll* a, ll* n, int size) {
-    if (size == 1) return *a;
-    ll tmp = modinverse(n[0], n[1]);
-    ll tmp2 = (tmp * (a[1] - a[0]) % n[1] + n[1]) % n[1];
-    ll ora = a[1];
-    ll tgcd = gcd(n[0], n[1]);
-    a[1] = a[0] + n[0] / tgcd * tmp2;
-    n[1] *= n[0] / tgcd;
-    ll ret = chinese_remainder(a + 1, n + 1, size - 1);
-    n[1] /= n[0] / tgcd;
-    a[1] = ora;
-    return ret;
-}
+// x = r_i mod m_i
+// (y, m) 'x = y mod m' 'm = lcm(m_i)', if not exists return (0, 0)
+auto crt = [](auto r, auto m) {
+  const int n = r.size();
+  i64 r0 = 0, m0 = 1;
+  for (int i = 0; i < n; i++) {
+    i64 r1 = r[i], m1 = m[i];
+    if (m0 < m1) swap(r0, r1), swap(m0, m1);
+    if (m0 % m1 == 0 && r0 % m1 != r1) return pair(0LL, 0LL);
+    if (m0 % m1 == 0) continue;
+    i64 g = gcd(m0, m1);
+    if ((r1 - r0) % g) return pair(0LL, 0LL);
+    i64 u0 = m0 / g, u1 = m1 / g;
+    i64 x = (r1 - r0) / g % u1 * modinv(u0, u1) % u1;
+    r0 += x * m0, m0 *= u1;
+    if (r0 < 0) r0 += m0;
+  }
+  return pair(r0, m0);
+};
